38d5aaf145c7ed89b961b85a11204f177baa711b
zni4 <38941259+zni4@users.noreply.github.com>
2020-01-23 11:29:34 +0100

    Añadimos distintas formas de mostrar la burbuja

---
+++ index.js
@@ -0,0 +1,50 @@
+function mostrarBurbuja() {
+    document.getElementById("comentario").classList.remove("invisible");
+    document.getElementById("comentario").classList.add("visible");
+}
+
+function ocultarBurbuja() {
+    document.getElementById("comentario").classList.add("invisible");
+    document.getElementById("comentario").classList.remove("visible");
+}
+
+
+function mostrarBurbuja2() {
+    document.getElementById("comentario2").classList.remove("oculto");
+    document.getElementById("comentario2").classList.add("visible");
+}
+
+function ocultarBurbuja2() {
+    document.getElementById("comentario2").classList.add("oculto");
+    document.getElementById("comentario2").classList.remove("visible");
+}
+
+function mostrarBurbuja3(id) {
+    
+    var divModal = document.getElementById('comentario3');
+
+    var comentario = document.getElementById('comentario3');
+
+    if (divModal != null) {
+        comentario.parentNode.removeChild(comentario);
+    }
+
+    //Obtenemos el contenedor y e incluimos el elemento
+    var divNodo = document.getElementById('contenedor_comentario_' + id);
+
+    var texto = document.getElementById(id).innerText;
+
+    divNodo.innerHTML += '<div id="comentario3" class="bubble_comment_left3"><div class="closeButton" onclick="ocultarBurbuja3();"><b>CERRAR &times</b></div>' + texto + '</div>';
+    
+    divModal = divNodo;
+}
+
+function ocultarBurbuja3() {
+    var divModal = document.getElementById('comentario3');
+
+    var comentario = document.getElementById('comentario3');
+
+    if (divModal != null) {
+        comentario.parentNode.removeChild(comentario);
+    }
+}
